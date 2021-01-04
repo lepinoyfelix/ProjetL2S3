335b4f4aab67108c6a364c5ddf5b0a5306b99026
33768 <feli.lepinoy@etu.univ-tours.fr>
2021-01-04 21:08:52 +0100

    Version 18.4

--- src/Controller/AccueilEtudiantController.java
+++ src/Controller/AccueilEtudiantController.java
@@ -1,14 +1,26 @@
 package Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class AccueilEtudiantController {
     @FXML
-    private Label LabelTest;
-    @FXML
-    private Button Button;
+    private Button BtnRetourConnexion;
+
+    public void retourConnexion(ActionEvent event) throws IOException {
+        Stage stage = (Stage) BtnRetourConnexion.getScene().getWindow();
+        stage.close();
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/Fxml/Connexion.fxml")));
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("Connexion");
+    }
 
 }
